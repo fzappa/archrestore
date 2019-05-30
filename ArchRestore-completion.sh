c3fda6aad1d9e91d5814415ac090b852683189d3
Alan <fzappa@users.noreply.github.com>
2019-05-30 08:54:23 -0300

    Update ArchRestore-completion.sh
    add --installGdrive

--- ArchRestore-completion.sh
+++ ArchRestore-completion.sh
@@ -8,7 +8,7 @@ _ArchRestore(){
     opts+="--restore --restoreArch --restoreAur "
     opts+="--installYay --installYayPkgs --confLiveCD --installLiveCD --installChroot "
     opts+="--installPkgs --confUser --installNvidia --installConky "
-    opts+="--restoreMyConf --confMirror --confSys --installLDM "
+    opts+="--restoreMyConf --confMirror --installGdrive --confSys --installLDM "
 
 	if [[ ${cur} == -* ]] ; then
 		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
