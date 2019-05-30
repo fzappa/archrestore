be77d2e5701108bd6dcb4e304168eef5b5db2cfd
Alan <fzappa@users.noreply.github.com>
2019-05-30 00:21:00 -0300

    Update ArchRestore-completion.sh

--- ArchRestore-completion.sh
+++ ArchRestore-completion.sh
@@ -4,11 +4,11 @@ _ArchRestore(){
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     opts="-b -ba -bu -r -ra -ru "
-    opts+="--backup --backuparch --backupaur "
-    opts+="--restore --restorearch --restoreaur "
+    opts+="--backup --backupArch --backupAur "
+    opts+="--restore --restoreArch --restoreAur "
     opts+="--installYay --installYayPkgs --confLiveCD --installLiveCD --installChroot "
     opts+="--installPkgs --confUser --installNvidia --installConky "
-    opts+="--restoreOpenBox --confMirrorBr --confSys --installLDM "
+    opts+="--restoreMyConf --confMirror --confSys --installLDM "
 
 	if [[ ${cur} == -* ]] ; then
 		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
