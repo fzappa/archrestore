1c3d47ffccd8bd0c337a6f3d9ea52d42490953dd
Alan <fzappa@users.noreply.github.com>
2019-06-03 16:48:19 -0300

    Add files via upload

--- ArchRestore.sh
+++ ArchRestore.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
-#
-# Arch Linux Restore Script
-# Alan Franco
-# github.com/fzappa/archrestore
-#
+#sudo dd if=iso of=/dev/sdX bs=1M status=progress oflag=direct conv=sync
+#https://www.archlinux.org/groups/x86_64/
+#https://linuxhint.com/install_xfce_manjaro_linux/
+#https://forum.manjaro.org/t/bootsplash-provided-by-the-kernel/34467/170
+#https://misc.flogisoft.com/bash/tip_colors_and_formatting
+#source ArchRestoreAutoComp.sh
 
-USER=""
+USER="alan"
 
 ### Install
-MBR=""  # /dev/sdX
-ROOT="" # /dev/sdX1
+MBR="/dev/sda"   # /dev/sdX
+ROOT="/dev/sda4" # /dev/sdX1
 MKFS="mkfs.ext4"
 
 ### Swap
-SWAP=""      # /dev/sdX3
-NEWSWAP="NO" # YES | NO
+SWAP="/dev/sdd3" # /dev/sdX3
+NEWSWAP="NO"     # YES | NO
 
 ### Locale
 KEYBOARD="br-abnt2"
@@ -22,12 +23,12 @@ LANG=(pt_BR.UTF-8 UTF-8) # "en_US.UTF-8 UTF-8" is default
 XKB="localectl set-x11-keymap br abnt2"
 
 ### Network and packages
-COUNTRY="country=BR"  # "country=BR&country=US"
-HOSTNAME="hostname"
-PACSTRAP="base"       # "base base-devel"
+COUNTRY="country=BR"       # "country=BR&country=US"
+HOSTNAME="urania"
+PACSTRAP="base base-devel" # "base base-devel"
 
 # Change to "YES" after adapting the script
-EDITED="NO" # YES | NO
+EDITED="YES" # YES | NO
 
 
 #############################################################
@@ -48,9 +49,11 @@ NC='\e[0m' # No Color
 if [ $EDITED == "YES" ]; then
 
 
-  ARCHPKG="Arch-pkglist.txt"
-  AURPKG="Aur-pkglist.txt"
   KEY="$1"
+
+  ## awk '{printf "%s"" ",$0}' Arch-pkglist.txt > pkgline.txt
+  # Run <./ArchRestore.sh --backupArch> on previous installed system and 
+  # paste here the list from file Arch-pkglist.txt.
 
 
 
@@ -59,10 +62,14 @@ if [ $EDITED == "YES" ]; then
   main(){
 
     # Load all functions
-    if [ -d "functions" ]; then
-      for f in functions/*.sh; do 
+    if [ -d "src" ]; then
+      for f in src/*.sh; do 
         source $f; 
       done
+    else
+      echo -e "${REDB}ERRO: src/ folder not found!${NC}"
+      echo -e "${RED}Unable to continue install.${NC}"
+      exit 1
     fi
 
     case $KEY in
@@ -193,7 +200,3 @@ else
   echo -e "\t${RED}!!! You MUST read and adapt the script before continue !!!${NC}\n\n"
   exit 1
 fi
-
-
-
-
