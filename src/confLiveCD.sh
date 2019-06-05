169ac6478422533f414c79fecd98c7926e49680e
fzappa <alanfr.cefetrj@gmail.com>
2019-06-05 00:29:35 -0300

    corrections

--- src/confLiveCD.sh
+++ src/confLiveCD.sh
@@ -19,31 +19,31 @@ confLiveCD(){ #begin --confLiveCD
     echo -e "${YELLOW}Now, creating partitions: ${NC}"
     
     if [ $NEWSWAP == "YES" ] && [[ -n "${SWAP//}" ]]; then # if yes and not empty
-      echo -e "${REDB}Formating $SWAP as swap partition ${NC}"
-      echo -e "${YELLOW}mkswap $SWAP${NC}"
-      mkswap $SWAP
-      echo -e "${YELLOW}swapon $SWAP${NC}"
-      swapon $SWAP
+        echo -e "${REDB}Formating $SWAP as swap partition ${NC}"
+        echo -e "${YELLOW}mkswap $SWAP${NC}"
+        mkswap $SWAP
+        echo -e "${YELLOW}swapon $SWAP${NC}"
+        swapon $SWAP
     elif [ $NEWSWAP == "NO" ] && [[ -n "${SWAP//}" ]]; then # if no and not empty
-      echo -e "${YELLOW}$SWAP will not be formated ${NC}"
-      echo -e "${YELLOW}Mounting $SWAP as swap partition ${NC}"
-      echo -e "${YELLOW}swapon $SWAP ${NC}"
-      swapon $SWAP
+        echo -e "${YELLOW}$SWAP will not be formated ${NC}"
+        echo -e "${YELLOW}Mounting $SWAP as swap partition ${NC}"
+        echo -e "${YELLOW}swapon $SWAP ${NC}"
+        swapon $SWAP
     elif [[ -z "${SWAP//}" ]]; then  # if empty
-      echo -e "${RED}SWAP will not be created ${NC}"
-      read -p "$(echo -e ${REDB}"Press any key to continue..."${NC})"
+        echo -e "${RED}SWAP will not be created ${NC}"
+        read -p "$(echo -e ${REDB}"Press any key to continue..."${NC})"
     fi
 
     if [[ -n "${ROOT//}" ]]; then # if not empty
-      echo -e "${REDB}Formating $ROOT with $MKFS ${NC}"
-      $MKFS $ROOT
-      echo -e "${YELLOW}mounting $ROOT on /mnt ${NC}"
-      mount $ROOT /mnt
-      echo -e "${YELLOW}Run: ${NC} ${RED}$0 --installLiveCD${NC}"
+        echo -e "${REDB}Formating $ROOT with $MKFS ${NC}"
+        $MKFS $ROOT
+        echo -e "${YELLOW}mounting $ROOT on /mnt ${NC}"
+        mount $ROOT /mnt
+        echo -e "${YELLOW}Run: ${NC} ${RED}$0 --installLiveCD${NC}"
     else
-      echo -e "${RED}ERRO: System will not be installed${NC}"
-      echo -e "${YELLOW}ROOT disk (/) not exists ${NC}"
-      read -p "$(echo -e ${REDB}"Press any key to exit..."${NC})"
-      exit 1
+        echo -e "${RED}ERRO: System will not be installed${NC}"
+        echo -e "${YELLOW}ROOT disk (/) not exists ${NC}"
+        read -p "$(echo -e ${REDB}"Press any key to exit..."${NC})"
+        exit 1
     fi     
   } #end --confLiveCD
