f7267214be1d56483a889df3333999243c2f781d
fzappa <alanfr@id.uff.br>
2020-05-22 12:55:52 -0300

    Update dhcp section to netctl

--- src/confNetwork.sh
+++ src/confNetwork.sh
@@ -1,8 +1,31 @@
-confNetwork(){ #begin --confNetwork
+confNetwork() { #begin --confNetwork
     echo -e "${YELLOW}Configure static address network...${NC}"
     roottest
-      
-    if [ $DHCP == "NO" ]; then 
+
+    if [ $DHCP == "YES" ]; then
+
+        echo -e "${YELLOW}Using DHCP.${NC}"
+        FILE="/etc/netctl/$IFACE"
+
+        echo -e "\t${YELLOW}Configure DHCP with netctl.${NC}"
+
+        echo "Description='A basic dhcp ethernet connection'" >$FILE
+        echo "Interface=$IFACE" >>$FILE
+        echo "Connection=ethernet" >>$FILE
+        echo "IP=dhcp" >>$FILE
+        echo "#DHCPClient=dhcpcd" >>$FILE
+        echo "#DHCPReleaseOnStop=no" >>$FILE
+        echo "## for DHCPv6" >>$FILE
+        echo "#IP6=dhcp" >>$FILE
+        echo "#DHCP6Client=dhclient" >>$FILE
+        echo "## for IPv6 autoconfiguration" >>$FILE
+        echo "#IP6=stateless" >>$FILE
+
+        echo -e "\t${YELLOW}Enable and start dhcpcd service...${NC}"
+        systemctl enable dhcpcd
+        systemctl start dhcpcd
+
+    elif [ $DHCP == "NO" ]; then
 
         echo -e "\t${RED}Disable and remove netctl...${NC}"
         systemctl stop netctl.service
@@ -17,20 +40,21 @@ confNetwork(){ #begin --confNetwork
         systemctl stop systemd-networkd.service
         ip address flush dev $IFACE
         echo -e "\t${YELLOW}Configure static IP with networkd.${NC}"
-        echo "[Match]" > $FILE
-        echo "Name=$IFACE" >> $FILE
-        echo "" >> $FILE
-        echo "[Network]" >> $FILE
-        echo "Address=$IP" >> $FILE
-        echo "Gateway=$GW" >> $FILE
-        echo "DNS=$DNS" >> $FILE
-        echo "DNS=8.8.8.8" >> $FILE
+        echo "[Match]" >$FILE
+        echo "Name=$IFACE" >>$FILE
+        echo "" >>$FILE
+        echo "[Network]" >>$FILE
+        echo "Address=$IP" >>$FILE
+        echo "Gateway=$GW" >>$FILE
+        echo "DNS=$DNS" >>$FILE
+        echo "DNS=8.8.8.8" >>$FILE
 
         echo -e "\t${YELLOW}Enable and start systemd-networkd service...${NC}"
         systemctl enable systemd-networkd.service
         systemctl start systemd-networkd.service
 
-    else
-        echo -e "${YELLOW}Using DHCP.${NC}"
-    fi     
-} #end --confNetwork
+    fi
+
+}
+
+#end --confNetwork
