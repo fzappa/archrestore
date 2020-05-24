3354ef42075d54755259b87993e5c8ede6f6cd6e
fzappa <alanfr@id.uff.br>
2020-05-23 23:19:16 -0300

    Update installXfce

--- src/installXfce.sh
+++ src/installXfce.sh
@@ -2,4 +2,5 @@ installXfce(){ #begin --instaXfce
     echo -e "${YELLOW}Install Xfce...${NC}"
     roottest
     pacman -S --needed xfce4 xfce4-goodies networkmanager network-manager-applet
+    echo -e "${YELLOW}Now install nvidia (--installNvidia) or xorg (pacman -S xorg)...${NC}"
 } #end --installXfce
