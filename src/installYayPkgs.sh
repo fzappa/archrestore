d6fccdeb2092b4e315365f603d4cbff790e109d1
fzappa <alanfr@id.uff.br>
2020-06-02 13:51:48 -0300

    Add packages do installYayPkgs

--- src/installYayPkgs.sh
+++ src/installYayPkgs.sh
@@ -1,18 +1,19 @@
 installYayPkgs(){ #begin --installYayPkgs
     echo -e "${YELLOW}Install Yay packages...${NC}"
+    echo -e "${YELLOW}Uncomment packages in src/installYayPkgs.sh...${NC}"
     notroottest
     installYay
-    yay -S --needed --noconfirm visual-studio-code-bin
-    yay -S --needed --noconfirm latex-beamer
-    yay -S --needed --noconfirm spotify
-    yay -S --needed --noconfirm obkey
-    yay -S --needed --noconfirm obmenu-generator
-    yay -S --needed --noconfirm openbox-themes
-    yay -S --needed --noconfirm conky-lua-nv
-    yay -S --needed --noconfirm picom
-    yay -S --needed --noconfirm polybar
-    yay -S --needed --noconfirm ttf-ms-fonts
-    yay -S --needed --noconfirm ttf-font-logos
+    #yay -S --needed --noconfirm visual-studio-code-bin
+    #yay -S --needed --noconfirm latex-beamer
+    #yay -S --needed --noconfirm spotify
+    #yay -S --needed --noconfirm obkey
+    #yay -S --needed --noconfirm obmenu-generator
+    #yay -S --needed --noconfirm openbox-themes
+    #yay -S --needed --noconfirm conky-lua-nv
+    #yay -S --needed --noconfirm picom
+    #yay -S --needed --noconfirm polybar
+    #yay -S --needed --noconfirm ttf-ms-fonts
+    #yay -S --needed --noconfirm ttf-font-logos
     #yay -S --needed --noconfirm masterpdfeditor-free
     #yay -S --needed --noconfirm microsoft-r-open
     #yay -S --needed --noconfirm skypeforlinux-stable-bin
